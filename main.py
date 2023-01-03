fc22d9f1187c0d77ed9ad84427450c48b3b2cdf7
ivancedo <ivancedo09@gmail.com>
2023-01-03 20:59:55 +0100

    V2 - Corrections
    V2 wasn't working correctly beceuse of two small mistakes, just corrected them.

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import os, time
 
+file = open("results.txt", "w")
 host = "8.8.8.8"
-period = 30
-f = open("results.txt", "w")
+period = 2
+active_msg = "Network Active"
+error_msg = "Network Error"
 
 def check_ping(host_ip):
     response = os.system("ping -n 1 " + host_ip)
     if response == 0:
-        return "Network Active"
+        return active_msg
     else:
-        return "Network Error"
+        return error_msg
 
 while True:
     time.sleep(period)
     now = time.localtime()
-    if check_ping(host) == "Network Active":
-        f.write(time.strftime("%d/%m/%Y %H:%M:%S", now) + "\t" + "Network Error" + "\n")
+    if check_ping(host) == error_msg:
+        file.write(time.strftime("%d/%m/%Y %H:%M:%S", now) + "\t" + error_msg + "\n")
     else:
         continue
